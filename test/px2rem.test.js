6b24484be0db4b46c11fc6d420730b7fa9c5f9b1
songsiqi <songsiqi2006@126.com>
2015-07-08 16:30:23 +0800

    refine test case

--- test/px2rem.test.js
+++ test/px2rem.test.js
@@ -4,37 +4,61 @@ var path = require('path');
 var fs = require('fs');
 var cssmin = require('cssmin');
 
-describe('px2rem', function() {
-    var px2remIns = new Px2rem({remUnit: 64});
+describe('px2rem should work with @2x origin css file', function() {
+    var px2remIns = new Px2rem(/*{remUnit: 75, baseDpr: 2}*/);
+    var srcPath = path.join(__dirname, 'assets/test.2x.css');
+    var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
 
     it('[default] should output right rem file', function() {
-        var srcPath = path.join(__dirname, 'assets/test.css');
         var outputPath = path.join(__dirname, 'output/default.rem.css');
-        var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
         var outputText = px2remIns.generateRem(srcText);
         assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
     });
 
     it('should output right @1x file', function() {
-        var srcPath = path.join(__dirname, 'assets/test.css');
         var outputPath = path.join(__dirname, 'output/default.1x.css');
-        var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
         var outputText = px2remIns.generateThree(srcText, 1);
         assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
     });
 
     it('should output right @2x file', function() {
-        var srcPath = path.join(__dirname, 'assets/test.css');
         var outputPath = path.join(__dirname, 'output/default.2x.css');
-        var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
         var outputText = px2remIns.generateThree(srcText, 2);
         assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
     });
 
     it('should output right @3x file', function() {
-        var srcPath = path.join(__dirname, 'assets/test.css');
         var outputPath = path.join(__dirname, 'output/default.3x.css');
-        var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
+        var outputText = px2remIns.generateThree(srcText, 3);
+        assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
+    });
+});
+
+describe('px2rem should work with @3x origin css file', function() {
+    var px2remIns = new Px2rem({remUnit: 112.5, baseDpr: 3});
+    var srcPath = path.join(__dirname, 'assets/test.3x.css');
+    var srcText = fs.readFileSync(srcPath, {encoding: 'utf8'});
+
+    it('[default] should output right rem file', function() {
+        var outputPath = path.join(__dirname, 'output/default.rem.css');
+        var outputText = px2remIns.generateRem(srcText);
+        assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
+    });
+
+    it('should output right @1x file', function() {
+        var outputPath = path.join(__dirname, 'output/default.1x.css');
+        var outputText = px2remIns.generateThree(srcText, 1);
+        assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
+    });
+
+    it('should output right @2x file', function() {
+        var outputPath = path.join(__dirname, 'output/default.2x.css');
+        var outputText = px2remIns.generateThree(srcText, 2);
+        assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
+    });
+
+    it('should output right @3x file', function() {
+        var outputPath = path.join(__dirname, 'output/default.3x.css');
         var outputText = px2remIns.generateThree(srcText, 3);
         assert.equal(cssmin(outputText), cssmin(fs.readFileSync(outputPath, {encoding: 'utf8'})));
     });
