1077ee2484d6288484b5f7347d3f1ef074fcc4dc
tangxiangmin <645234650@qq.com>
2022-01-05 22:52:02 +0800

    fix: 处理.5px等忽略小数首位0时rem计算异常的问题

--- test/px2rem.test.js
+++ test/px2rem.test.js
@@ -64,3 +64,32 @@ describe('should work with @3x origin css file', function () {
     assert.equal(outputText, fs.readFileSync(expectedPath, {encoding: 'utf8'}));
   });
 });
+
+describe('Px2rem.prototype._getCalcValue rem', function () {
+  var px2remIns = new Px2rem({remUnit: 100});
+  it('normal number px', function () {
+    var outputText = px2remIns._getCalcValue('rem', '10px 20px')
+    assert.equal('0.1rem 0.2rem', outputText)
+  })
+
+  it('float number px', function () {
+    var outputText = px2remIns._getCalcValue('rem', '0.5px solid #000')
+    assert.equal('0.005rem solid #000', outputText)
+    var outputText2 = px2remIns._getCalcValue('rem', '0.5px 11.5px')
+    assert.equal('0.005rem 0.115rem', outputText2)
+  })
+
+  it('float number px ignore first zero', function () {
+    var outputText =  px2remIns._getCalcValue('rem', '.5px solid #000')
+    assert.equal('0.005rem solid #000', outputText)
+
+    var outputText2 = px2remIns._getCalcValue('rem', ' .5px solid #000')
+    assert.equal(' 0.005rem solid #000', outputText2)
+
+    var outputText3 = px2remIns._getCalcValue('rem', 'solid .5px #000')
+    assert.equal('solid 0.005rem #000', outputText3)
+
+    var outputText4 = px2remIns._getCalcValue('rem', 'solid #000 .5px')
+    assert.equal('solid #000 0.005rem', outputText4)
+  })
+});
